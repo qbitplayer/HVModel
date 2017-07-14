edb536cfe57fa46cfa1a71f662bef68b4a9db7f4
alumne <alumne@PC05S21>
2017-07-14 11:33:50 +0200

    DBConnector Remove added TestRemove added

--- HVModel/src/dao/DBConnector.java
+++ HVModel/src/dao/DBConnector.java
@@ -1,6 +1,9 @@
 package dao;
 
 import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
 
 import model.Mascota;
 import model.Person;
@@ -13,7 +16,8 @@ public class DBConnector  extends DBManager implements HVServices{
 	// Miquel 
 	@Override
 	public void insert(Person owner, Mascota mascota) {
-		// TODO Auto-generated method stub
+
+		
 	}
 
 	// Jordi 
@@ -24,10 +28,17 @@ public class DBConnector  extends DBManager implements HVServices{
 	}
 
 	//Jordi //Luis
+	
+	
 	@Override
-	public void remove(Person owner) {
-		// TODO Auto-generated method stub
-		
+	public void remove(Person owner) {		
+		connect();
+			EntityManager entiManager = getEntityManager();		
+			entiManager.getTransaction().begin();						
+			Person  recovered = entiManager.find(Person.class, owner.getId());
+			entiManager.remove(recovered);
+			entiManager.getTransaction().commit();			
+		close();		
 	}
 
 	//Toni 
@@ -54,15 +65,20 @@ public class DBConnector  extends DBManager implements HVServices{
 	//Luis 
 	@Override
 	public HashSet<Mascota> findMascota(int idPerson) {
-		// TODO Auto-generated method stub
-		return null;
+		connect();
+		HashSet<Mascota> pet = new HashSet<Mascota>();
+		pet.add( this.getEntityManager().find(Mascota.class, idPerson));
+		close();
+		return pet;
 	}
 
 	//Dorian 
 	@Override
-	public HashSet<Mascota> selectAllPerson() {
-		// TODO Auto-generated method stub
-		return null;
+	public HashSet<Person> selectAllPerson() { 
+		//connect();
+		//	HashSet<Person> list = new HashSet<>(selectAll(Person.class));
+		//close();
+		return null;//list;
 	}
 
 	//Dorian 
